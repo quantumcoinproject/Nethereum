524aed2d14ea1c0886be16d47cfc38b736295d82
Doge Protocol Platform <83708066+DogeProtocol@users.noreply.github.com>
2024-04-07 17:57:29 -0700

    Update publishnuget.bat publish nupkg first before symbols

--- src/publishnuget.bat
+++ src/publishnuget.bat
@@ -1,2 +1,3 @@
+for /R %DIR% %%a in (*.nupkg) do  dotnet nuget push "%%a" --api-key %1 --source https://api.nuget.org/v3/index.json
 for /R %DIR% %%a in (*.snupkg) do  dotnet nuget push "%%a" --api-key %1 --source https://api.nuget.org/v3/index.json
 
